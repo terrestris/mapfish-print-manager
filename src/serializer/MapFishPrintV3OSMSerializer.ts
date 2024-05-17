b4bd005645dd52149a420df866649a40c94ce6c0
faouziH21 <2004homsani@gmail.com>
2024-05-17 14:46:12 +0200

    fix: fixes XYZ layer so it prints and simplifies OSM serialzer

--- src/serializer/MapFishPrintV3OSMSerializer.ts
+++ src/serializer/MapFishPrintV3OSMSerializer.ts
@@ -11,29 +11,14 @@ export class MapFishPrintV3OSMSerializer extends MapFishPrintV3XYZSerializer {
 
   serialize(olLayer: OlLayer, opts?: any) {
     const source = olLayer.getSource();
-    const optsToApply = {
-      baseURL: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      customParams: {},
-      dpi: 72,
-      failOnError: false,
-      imageExtension: 'png',
-      maxExtent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
-      rasterStyle: undefined,
-      tileSize: [256, 256],
-      ...opts
-    };
 
     if (!source || !this.validateSource(source)) {
       return;
     }
 
-    const urls = source.getUrls();
-    const baseUrl = urls ? urls[0] : undefined;
-
     return {
       ...super.serialize(olLayer, opts),
-      baseURL: baseUrl,
-      ...optsToApply
+      baseURL: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
     };
   }
 }
