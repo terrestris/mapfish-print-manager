dbe15bb58685404a68f79381b90fb2cbe8d8045b
Andre Henn <henn@terrestris.de>
2024-10-21 11:08:11 +0200

    chore: update babel version

--- babel.config.cjs
+++ babel.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties'
+    ['@babel/plugin-transform-class-properties', { loose: false }]
   ]
 };
