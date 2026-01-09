734c7c9cba28be6460eb8fa8a1fc97be8a10277f
Noémie Treff <ntreff@terrestris.de>
2026-01-09 10:25:14 +0100

    fix: wmts serializer

--- src/serializer/MapFishPrintV3WMTSSerializer.ts
+++ src/serializer/MapFishPrintV3WMTSSerializer.ts
@@ -59,7 +59,7 @@ export class MapFishPrintV3WMTSSerializer implements BaseSerializer {
       customParams: undefined,
       dimensionParams,
       dimensions,
-      failOnError: opts?.failOnError || true,
+      failOnError: false,
       imageFormat: source.getFormat() || 'image/png',
       layer: source.getLayer(),
       matrices: tileGrid.getMatrixIds().map((matrixId, index) => ({
@@ -68,7 +68,7 @@ export class MapFishPrintV3WMTSSerializer implements BaseSerializer {
           ? [matrixSizes[index][0], matrixSizes[index][1]]
           : [Math.pow(2, index), Math.pow(2, index)],
         scaleDenominator: scaleDenominators[index],
-        tileSize: [tileGrid.getTileSize(index), tileGrid.getTileSize(index)],
+        tileSize: tileGrid.getTileSize(index),
         topLeftCorner: tileGrid.getOrigin(index) || tileGrid.getOrigin(0)
       })),
       matrixSet: source.getMatrixSet(),
